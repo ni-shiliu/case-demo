fa38c0cf7a22425f85e93992d1cce411db971a4e
nihao <nihao@za-doctor.com>
2023-06-02 10:33:04 +0800

    build(init): init

---
+++ neil-spring-statemachine/src/main/kotlin/com/neil/neilspringstatemachine/NeilSpringStatemachineApplication.kt
@@ -0,0 +1,11 @@
+package com.neil.neilspringstatemachine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeilSpringStatemachineApplication
+
+fun main(args: Array<String>) {
+    runApplication<NeilSpringStatemachineApplication>(*args)
+}
---
+++ neil-spring-statemachine/src/test/kotlin/com/neil/neilspringstatemachine/NeilSpringStatemachineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.neil.neilspringstatemachine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeilSpringStatemachineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
